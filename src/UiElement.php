13ee11718ab6f9cc9e1372b564183ebb0303d024
Brooke Bryan <brooke@bajb.net>
2018-12-03 13:06:11 +0000

    Ui Element

---
+++ src/UiElement.php
@@ -0,0 +1,42 @@
+<?php
+namespace Packages\Ui;
+
+class UiElement implements Renderable
+{
+  protected $_templateFilePath;
+
+  protected function getTemplateFilePath()
+  {
+    if($this->_templateFilePath === null)
+    {
+      $this->_templateFilePath = substr(__FILE__, -3) . 'phtml';
+    }
+    return $this->_templateFilePath;
+  }
+
+  /**
+   * Build the view response with the relevant template file
+   *
+   * @return string
+   * @throws \Exception
+   */
+  public function render(): string
+  {
+    $tpl = $this->getTemplateFilePath();
+    if(!file_exists($tpl))
+    {
+      throw new \Exception("The template file '$tpl' does not exist", 404);
+    }
+    ob_start();
+    try
+    {
+      include $tpl;
+    }
+    catch(\Exception $e)
+    {
+      ob_end_clean();
+      throw $e;
+    }
+    return ob_get_clean();
+  }
+}
